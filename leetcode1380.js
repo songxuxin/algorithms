9c6eca1671b6badd23d0d51eabd1f2622c286a18
Xavier Song <xaviersong@Xaviers-MacBook-Pro.local>
2020-04-16 11:31:00 -0700

    add qs

---
+++ leetcode1380.js
@@ -0,0 +1,63 @@
+// Given a m * n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
+
+// A lucky number is an element of the matrix such that it is the minimum element in its row and maximum in its column.
+
+ 
+
+// Example 1:
+
+// Input: matrix = [[3,7,8],[9,11,13],[15,16,17]]
+// Output: [15]
+// Explanation: 15 is the only lucky number since it is the minimum in its row and the maximum in its column
+// Example 2:
+
+// Input: matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+// Output: [12]
+// Explanation: 12 is the only lucky number since it is the minimum in its row and the maximum in its column.
+// Example 3:
+
+// Input: matrix = [[7,8],[1,2]]
+// Output: [7]
+ 
+
+// Constraints:
+
+// m == mat.length
+// n == mat[i].length
+// 1 <= n, m <= 50
+// 1 <= matrix[i][j] <= 10^5.
+// All elements in the matrix are distinct.
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var luckyNumbers  = function(matrix) {
+    let res = [];
+    let max = 0;
+    let flag = false;
+    for(let i=0; i<matrix.length; i++){
+        let j = 0;
+        let min = matrix[i][j];
+        let minIndex = 0;
+        for( ; j<matrix[i].length; j++){
+            if(matrix[i][j] < min){
+                min = matrix[i][j];
+                minIndex = j;
+            }
+        }
+        for(let k=0; k<matrix.length; k++){
+            let max = matrix[i][minIndex];
+            if(matrix[k][minIndex] > max){
+                flag = false;
+                break;
+            }else{
+                flag = true;
+            }
+        }
+        if(flag){
+            res.push(matrix[i][minIndex]); 
+        }    
+    }
+    return res;
+};
