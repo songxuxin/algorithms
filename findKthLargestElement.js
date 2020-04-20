51cde048b3148ca9500bcdcb31dd47e63aedb2a8
bob <bob@>
2020-04-19 23:26:25 -0700

    find kth largest element, find 3rd & find 2nd largest element also

--- findKthLargestElement.js
+++ findKthLargestElement.js
@@ -41,4 +41,57 @@ var swap = function(nums, i, j) {
     nums[j] = temp;
 };
 
-console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 3)) //5
+console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 3)) //5 
+
+//if just need 3rd or 2nd largest element, no need to sort
+//just keep 3 or 2 variables to memorize the 1st, 2nd, and 3rd largest elements;
+
+var find3rdLargest = function(nums) {
+    if(nums === null || nums.length < 3) {
+        return;
+    }
+    let first = Math.max(Math.max(nums[0], nums[1]), nums[2]);
+    let third = Math.min(Math.min(nums[0], nums[1]), nums[2]);
+    let second = nums[0] + nums[1] + nums[2] - first - third;
+    console.log("before loop: first=", first, "second=", second, "third=", third);
+    for(let i = 3; i < nums.length; i++) {
+        let cur = nums[i];
+        if(cur >= first) {
+            third = second;
+            second = first;
+            first = cur;
+        } else if(cur >= second) {
+            third = second;
+            second = cur;
+        } else if(cur >= third) {
+            third = cur;
+        }
+        console.log("first=", first, "second=", second, "third=", third);
+    }
+    return third;
+}
+
+console.log(find3rdLargest([3,2,3,1,2,4,5,5,6])) //5
+
+var find2ndLargest = function(nums) {
+    if(nums === null || nums.length < 2) {
+        return;
+    }
+    let first = Math.max(nums[0], nums[1]);
+    let second = Math.min(nums[0], nums[1]);
+    
+    console.log("before loop: first=", first, "second=", second);
+    for(let i = 2; i < nums.length; i++) {
+        let cur = nums[i];
+        if(cur >= first) {
+            second = first;
+            first = cur;
+        } else if(cur >= second) {
+            second = cur;
+        } 
+        console.log("i=", i, "first=", first, "second=", second);
+    }
+    return second;
+}
+
+console.log(find2ndLargest([3,2,3,1,2,4,5,5,6,7])) //6
