3a6ac86f9d7de93faef412c4c92e18825cdc2b23
Xavier Song <xaviersong@Xaviers-MacBook-Pro.local>
2020-04-25 23:58:27 -0700

    update factorial dp

--- factorial.js
+++ factorial.js
@@ -15,3 +15,34 @@ let printFactorial = function(n) {
 
 
 console.log(printFactorial(5));
+console.log(printFactorial(100));
+
+
+
+let dpFactorial = function(n, arr){
+	let res = null;
+	if(arr[n] != undefined){
+		return arr[n];
+	}
+
+	if(n === 1 ){
+		res = 1;
+	}else if(n === 0){
+		res = 0;
+	}else{
+		res = n * dpFactorial(n-1, arr);
+	}
+
+	arr[n] = res;
+
+	return res;
+}
+
+let memo = function(n){
+	let arr = new Array(n);
+	return dpFactorial(n, arr);
+}
+
+
+console.log(memo(5));
+console.log(memo(100));
